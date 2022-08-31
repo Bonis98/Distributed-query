73823f12247ad913eece4d2cad4e4c94c77c6fdd
Marco <Bonis98@gmail.com>
2022-08-31 15:48:35 +0200

    Implemented class node

--- node.py
+++ node.py
@@ -1,3 +1,9 @@
 class Node:
-    def __int__(self, operation):
+    permitted_ops = ['projection', 'selection', 'cartesian', 'join',
+                     'group by', 'encryption', 'decryption', 're-encryption']
+
+    def __init__(self, operation, attributes):
+        if operation not in self.permitted_ops:
+            raise ValueError("results: status must be one of %r." % self.permitted_ops)
         self.operation = operation
+        self.attributes = attributes
