a31f01260ed9b6d2fd9a5be23e4ba04079807313
Marco <Bonis98@gmail.com>
2022-08-31 17:10:04 +0200

    Added type checking

--- node.py
+++ node.py
@@ -5,9 +5,11 @@ class Ops:
     permitted_ops = ['projection', 'selection', 'cartesian', 'join',
                      'group by', 'encryption', 'decryption', 're-encryption']
 
-    def __init__(self, operation, attributes):
+    def __init__(self, operation, attributes: set):
         if operation not in self.permitted_ops:
-            raise ValueError("results: status must be one of %r." % self.permitted_ops)
+            raise ValueError("Ops: operation must be one of %r." % self.permitted_ops)
+        if not isinstance(attributes, set):
+            raise TypeError("Ops: attributes must be a set, not %s" % type(attributes))
         self.operation = operation
         self.attributes = attributes
 
