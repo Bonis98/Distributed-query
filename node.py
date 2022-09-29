5077065fe02aa79a296e73b5e612c2bf68c5b2f9
Marco <Bonis98@gmail.com>
2022-09-29 08:44:21 +0200

    Fixed errors in profile computations

--- node.py
+++ node.py
@@ -3,7 +3,7 @@ from anytree import NodeMixin
 
 # Class representing an operation to be inserted in the tree plan
 class Ops:
-    def __init__(self, operation, Ap: set, Ae: set, enc_attr: set, group_attr=None):
+    def __init__(self, operation, Ap: set, Ae: set, enc_attr: set, group_attr, select_multi_attr):
         # Value restriction for operation attribute
         permitted_ops = [
             'projection', 'selection', 'cartesian', 'join',
@@ -13,6 +13,10 @@ class Ops:
         enc_attr = set(enc_attr)
         if operation not in permitted_ops:
             raise ValueError('Ops: operation must be one of %r.' % permitted_ops)
+        if operation == 'selection':
+            self.select_multi_attr = select_multi_attr
+        else:
+            self.select_multi_attr = False
         if len(Ap.intersection(Ae).intersection(enc_attr)):
             raise ValueError('Ops: plain, re_enc and enc sets must be disjoint')
         if group_attr is not None:
@@ -59,8 +63,8 @@ class Node(Ops, NodeMixin):
 
     def __init__(
             self, operation, Ap: set, Ae: set, enc_attr: set, size=None, re_encryption=False,
-            print_label=None, group_attr=None, parent=None, children=None):
-        super().__init__(operation, Ap, Ae, enc_attr, group_attr)
+            print_label=None, group_attr=None, select_multi_attr=False, parent=None, children=None):
+        super().__init__(operation, Ap, Ae, enc_attr, group_attr, select_multi_attr)
         self.parent = parent
         self.re_encryption = re_encryption
         if operation != 're-encryption':
@@ -77,9 +81,9 @@ class Node(Ops, NodeMixin):
     def compute_profile(self):
         # If node is a leaf, all attributes are encrypted in storage
         if self.is_leaf:
-            self.vp = set()
-            self.ve = set()
-            self.vE = self.Ap.union(self.Ae).union(self.enc_attr)
+            self.vp = self.Ap
+            self.ve = self.Ae
+            self.vE = set(self.relation.attributes).difference(self.Ap).difference(self.Ae)
             self.ip = set()
             self.ie = set()
             self.eq = set()
@@ -90,33 +94,41 @@ class Node(Ops, NodeMixin):
                 self.__assign_profile(n[0], n[1])
             else:
                 self.__assign_profile(n[0])
-        if len(self.Ap) > 0:
-            self.vp = self.vp.union(self.Ap)
-            self.ve = self.ve.difference(self.Ap)
-            self.vE = self.vE.difference(self.Ap)
-        if len(self.Ae) > 0:
-            self.ve = self.ve.union(self.Ae)
-            self.vE = self.vE.difference(self.Ae)
         if self.operation == 'projection':
             self.vp = self.vp.intersection(self.Ap)
             self.ve = self.ve.intersection(self.Ae)
             self.vE = self.vE.intersection(self.enc_attr)
-        elif self.operation == 'selection' and self.num_attr() == 1:
+        elif self.operation == 'selection' and not self.select_multi_attr:
             self.ip = self.ip.union(self.vp.intersection(self.Ap))
             self.ie = self.ve.union(self.vE).intersection(self.Ae.union(self.enc_attr)).union(self.ie)
-        elif self.operation == 'selection' and self.num_attr() == 2:
-            self.eq.add(frozenset(self.Ap.union(self.Ae).union(self.enc_attr)))
+        elif self.operation == 'selection' and self.select_multi_attr:
+            if len(self.Ap):
+                self.eq.add(frozenset(self.Ap))
+            if len(self.Ae):
+                self.eq.add(frozenset(self.Ae))
+            if len(self.enc_attr):
+                self.eq.add(frozenset(self.enc_attr))
         elif self.operation == 'cartesian':
             # Union of sets of children, already done by __assign_profile
             pass
         elif self.operation == 'join':
             self.eq.add(frozenset(self.Ap.union(self.Ae).union(self.enc_attr)))
         elif self.operation == 'group-by':
-            self.vp = self.vp.intersection(self.Ap)
-            self.ve = self.ve.intersection(self.Ae)
-            self.vE = self.vE.intersection(self.enc_attr)
+            self.vp = self.vp.intersection(self.Ap.union(set(self.group_attr)))
+            self.ve = self.ve.intersection(self.Ae.union(set(self.group_attr)))
+            self.vE = self.vE.intersection(self.enc_attr.union(set(self.group_attr)))
             self.ip = self.vp.intersection(self.group_attr).union(self.ip)
             self.ie = self.ve.union(self.vE).intersection(self.group_attr).union(self.ie)
+        elif self.operation == 'encryption':
+            self.vp = self.vp.difference(self.Ap)
+            self.ve = self.ve.union(self.Ap)
+        elif self.operation == 'decryption':
+            self.vp = self.vp.union(self.Ae)
+            self.ve = self.ve.difference(self.Ae)
+            self.vE = self.vE.difference(self.Ae)
+        elif self.operation == 're-encryption':
+            self.ve = self.ve.union(self.Ae)
+            self.vE = self.vE.difference(self.Ae)
 
     def __assign_profile(self, n1, n2=None):
         # Retrieves profile from child(ren) node(s)
