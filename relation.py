47deb67d72f5e3a5cf2a4ec369ead818b4b117a8
Marco <Bonis98@gmail.com>
2022-10-17 14:58:04 +0200

    Added relation name

--- relation.py
+++ relation.py
@@ -1,5 +1,6 @@
 class Relation:
-    def __init__(self, storage_provider, attributes: list, enc_costs: list, dec_costs: list, size: list):
+    def __init__(self, name, storage_provider, attributes: list, enc_costs: list, dec_costs: list, size: list):
+        self.name = name
         self.storage_provider = storage_provider
         if not (len(attributes) == len(enc_costs) == len(dec_costs) == len(size)):
             raise ValueError('Relation: attributes, enc_costs dec_costs and size must have the same length')
